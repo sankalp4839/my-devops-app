78d5a88f9719e96069788dd47681da03e07607cb
sankalp4839 <sankalp.agnihotry@gmail.com>
2026-04-21 19:26:24 +0530

    Devops Initial setup

---
+++ app.py
@@ -0,0 +1,11 @@
+# a) basic flask app -> b) containerize it ->c) automate the build (using git) ->d) deploy to cloud (AWS EC2)
+
+from flask import Flask
+app =Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Devops Successfull!</h1><p> Pythin + Docker + Git + AWS running.</p>"
+
+if __name__== "__main__":
+    app.run(host='0.0.0.0',port=5000)
